5882f3e02cafac4c7022b41c04cce569f8826c6a
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-27 14:06:51 +0300

    corrections

--- build/docker/hadoop/build.sh
+++ build/docker/hadoop/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 eval "$(ssh-agent -s)"
-java -v
+readlink -f $(which java)
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa -f -y
 chmod 0600 ~/.ssh/authorized_keys
 cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_key
