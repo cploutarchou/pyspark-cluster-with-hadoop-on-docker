35e07beff8b5fc5f184163aa086076673d00c775
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-28 01:06:32 +0300

    corrections

--- build/docker/hadoop/build.sh
+++ build/docker/hadoop/build.sh
@@ -4,4 +4,4 @@ chmod 0600 /root/.ssh/authorized_keys
 ssh-keygen -q -t rsa -N '' -f /root/.ssh/ssh/id_rsa <<<y 2>&1 >/dev/null
 cat /root/.ssh/id_rsa.pub >>/root/.ssh/authorized_key
 eval "$(ssh-agent -s)"
-start-all.sh
+start-all.sh hdfs dfs -mkdir /user hdfs dfs -mkdir /user/hadoop
