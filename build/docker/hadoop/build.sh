890cc4a6ef9c95d19b4584c55d4d1142d69e0261
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-28 02:03:15 +0300

    update build.sh

--- build/docker/hadoop/build.sh
+++ build/docker/hadoop/build.sh
@@ -2,15 +2,16 @@
 # Run ssh on logging, only if not running yet
 ACTIVE=$(pgrep -f /usr/sbin/sshd)
 if [ ! "$ACTIVE" ]; then
-    echo "starting sshd....";
+  echo "starting sshd...."
 
-    echo "successfully started PID = $(pgrep -f /usr/sbin/sshd)";
+  echo "successfully started PID = $(pgrep -f /usr/sbin/sshd)"
 else
-    echo "ssh service already started PID=$ACTIVE"
+  echo "ssh service already started PID=$ACTIVE"
 fi
-mkdir -p /root/.ssh/authorized_keys
-chmod 0600 /root/.ssh/authorized_keys
-ssh-keygen -q -t rsa -N '' -f /root/.ssh/ssh/id_rsa <<<y 2>&1 >/dev/null
-cat /root/.ssh/id_rsa.pub >>/root/.ssh/authorized_key
+mkdir -p ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa11 <<<y
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_key
 eval "$(ssh-agent -s)"
 start-all.sh hdfs dfs -mkdir /user hdfs dfs -mkdir /user/hadoop
