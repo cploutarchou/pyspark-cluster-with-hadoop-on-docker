9183d69dae6c277ff9403ce228f8ae85fad53195
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-28 00:06:16 +0300

    corrections

--- build/docker/hadoop/build.sh
+++ build/docker/hadoop/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-eval "$(ssh-agent -s)"
 mkdir -p /root/.ssh/authorized_keys
 chmod 0600 /root/.ssh/authorized_keys
 ssh-keygen -q -t rsa -N '' -f /root/.ssh/ssh/id_rsa <<<y 2>&1 >/dev/null
 cat /root/.ssh/id_rsa.pub >>/root/.ssh/authorized_key
+eval "$(ssh-agent -s)"
 hdfs namenode -format
 start-dfs.sh && start-yarn.sh && hdfs dfs -mkdir -p /user/root
-hdfs dfs -put "$HADOOP_HOME"/etc/hadoop/ input
+hdfs dfs -put /usr/local/hadoop/etc/hadoop/ input
