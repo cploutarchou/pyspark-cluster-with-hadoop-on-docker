ab1ed6e4dd59fe46bb9d2f4bb509560f445db2df
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-29 00:00:08 +0300

    CORRECTIONS

--- build/docker/hadoop/bootstrap.sh
+++ build/docker/hadoop/bootstrap.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
+HADOOP_PREFIX:=/usr/local/hadoop
 
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+"$HADOOP_PREFIX"/etc/hadoop/hadoop-env.sh
 
 rm /tmp/*.pid
 
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+# shellcheck disable=SC2164
+cd "$HADOOP_PREFIX"/share/hadoop/common
+for cp in ${ACP//,/ }; do
+  echo == $cp
+  curl -LO "$cp"
+done
+cd - || exit
 
 # altering the core-site configuration
-sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
-
+sed s/HOSTNAME/"$HOSTNAME"/ /usr/local/hadoop/etc/hadoop/core-site.xml.template >/usr/local/hadoop/etc/hadoop/core-site.xml
 
 service sshd start
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
-$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
+"$HADOOP_PREFIX"/sbin/start-dfs.sh
+"$HADOOP_PREFIX"/sbin/start-yarn.sh
+"$HADOOP_PREFIX"/sbin/mr-jobhistory-daemon.sh start historyserver
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
