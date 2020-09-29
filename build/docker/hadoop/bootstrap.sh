62e4a65bb09715aac55810e0757ba06d6cb44ca2
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-29 19:31:01 +0300

    corrections

--- build/docker/hadoop/bootstrap.sh
+++ build/docker/hadoop/bootstrap.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-HADOOP_PREFIX:=/usr/local/hadoop
-
-sed s/HOSTNAME/"$HOSTNAME"/ /usr/local/hadoop/etc/hadoop/core-site.xml.template >/usr/local/hadoop/etc/hadoop/core-site.xml
+HADOOP_PREFIX=/usr/local/hadoop
 
 service sshd start
 "$HADOOP_PREFIX"/sbin/start-dfs.sh
 "$HADOOP_PREFIX"/sbin/start-yarn.sh
 "$HADOOP_PREFIX"/sbin/mr-jobhistory-daemon.sh start historyserver
-
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
-fi
-
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
-fi
