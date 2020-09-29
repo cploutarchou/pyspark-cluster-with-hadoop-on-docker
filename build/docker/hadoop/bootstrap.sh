5a2d3815d2b0c80101a84755108627a7c4b7c9d7
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-29 14:13:35 +0300

    corrections

--- build/docker/hadoop/bootstrap.sh
+++ build/docker/hadoop/bootstrap.sh
@@ -2,20 +2,6 @@
 
 HADOOP_PREFIX:=/usr/local/hadoop
 
-"$HADOOP_PREFIX"/etc/hadoop/hadoop-env.sh
-
-rm /tmp/*.pid
-
-# installing libraries if any - (resource urls added comma separated to the ACP system variable)
-# shellcheck disable=SC2164
-cd "$HADOOP_PREFIX"/share/hadoop/common
-for cp in ${ACP//,/ }; do
-  echo == $cp
-  curl -LO "$cp"
-done
-cd - || exit
-
-# altering the core-site configuration
 sed s/HOSTNAME/"$HOSTNAME"/ /usr/local/hadoop/etc/hadoop/core-site.xml.template >/usr/local/hadoop/etc/hadoop/core-site.xml
 
 service sshd start
