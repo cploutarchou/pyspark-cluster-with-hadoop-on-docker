d926287eecf9ab0444d7e81516bf975103d51562
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-29 19:58:41 +0300

    apply some corrections

--- build/docker/hadoop/bootstrap.sh
+++ build/docker/hadoop/bootstrap.sh
@@ -2,10 +2,6 @@
 
 HADOOP_PREFIX=/usr/local/hadoop
 
-ufw allow ssh
-systemctl restart ssh.service
-mkdir /usr/local/hadoop-2.10.1/logs/
-chmod -rwxr-xr-x /usr/local/hadoop-2.10.1/logs/
-"$HADOOP_PREFIX"/sbin/start-dfs.sh
-"$HADOOP_PREFIX"/sbin/start-yarn.sh
+
+"$HADOOP_PREFIX"/sbin/start-all.sh
 "$HADOOP_PREFIX"/sbin/mr-jobhistory-daemon.sh start historyserver
