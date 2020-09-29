b7c94a5cf50e0b22781550f1531c55289b779ccf
Christos Ploutarchou <cploutarchou@gmail.com>
2020-09-29 19:45:09 +0300

    update boostrap.sh

--- build/docker/hadoop/bootstrap.sh
+++ build/docker/hadoop/bootstrap.sh
@@ -2,7 +2,10 @@
 
 HADOOP_PREFIX=/usr/local/hadoop
 
-service sshd start
+ufw allow ssh
+systemctl restart ssh.service
+mkdir /usr/local/hadoop-2.10.1/logs/
+chmod -rwxr-xr-x /usr/local/hadoop-2.10.1/logs/
 "$HADOOP_PREFIX"/sbin/start-dfs.sh
 "$HADOOP_PREFIX"/sbin/start-yarn.sh
 "$HADOOP_PREFIX"/sbin/mr-jobhistory-daemon.sh start historyserver
